4e28f4472f2455d1c3ac46678135da7d939b5d56
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-08-28 23:51:00 -0300

    cambio BASE_URL ApiUtilsLogin.java

--- app/src/main/java/com/recitales/app/models/ApiUtilsLogin.java
+++ app/src/main/java/com/recitales/app/models/ApiUtilsLogin.java
@@ -3,7 +3,7 @@ package com.recitales.app.models;
 import com.recitales.app.interfaces.UserServiceLogin;
 
 public class ApiUtilsLogin {
-    public static final String BASE_URL = "https://vasalaapp.herokuapp.com/";
+    public static final String BASE_URL = "https://salty-bayou-21277.herokuapp.com/";
 
 
     public static UserServiceLogin getUserServiceLogin1(){
